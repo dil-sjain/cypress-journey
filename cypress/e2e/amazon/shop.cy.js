ba45fb2a05a96927c46604a92db302e56dea7b82
sshukla345 <sshukla@diligent.com>
2025-04-30 16:20:56 +0530

    Updated code for Login and add item to cart

--- cypress/e2e/amazon/shop.cy.js
+++ cypress/e2e/amazon/shop.cy.js
@@ -1,6 +1,3 @@
-
-import 'cypress-xpath'
-
 describe('amazon cart shopping',()=>{
     beforeEach(() => {
         cy.visit('https://www.amazon.in');
@@ -17,12 +14,12 @@ describe('amazon cart shopping',()=>{
         cy.get('#nav-logo-sprites').should('be.visible');//verify logo is visible after successful login
     });
     it('add to cart',()=>{
-        cy.get('.nav-search-field').type('iphone 15')
+        cy.get('.nav-search-field').type('ps5')
         cy.get('#nav-search-submit-button').click();
         cy.wait(2000)
-        cy.get('h2[aria-label="Apple iPhone 15 (128 GB) - Black"]').parents("a").invoke("removeAttr","target").click()
+        cy.get('h2[aria-label="Sony PlayStation5 Gaming Console (Slim)"]').parents("a").invoke("removeAttr","target").click()
         cy.wait(2000)
-        cy.xpath("//div[@id='buybackAddToCart']//input[@id='add-to-cart-button']").click({force:true});
+        cy.get('#add-to-cart-button').click();
+        cy.wait(2000);
     })
-
 });
