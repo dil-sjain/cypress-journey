c221901f95f2be9bdf55edef78b3a3baa40b82e7
satishtaji07 <satish.taji07@gmail.com>
2025-05-02 16:40:22 +0530

    Updated as per review comments

--- cypress/e2e/assignOne.cy.js
+++ cypress/e2e/assignOne.cy.js
@@ -17,11 +17,11 @@ describe('Assignment 1', () => {
         
     })
 
-    it.skip('Login to Amazon', function() {
+    it('Login to Amazon', function() {
         
         const expectedLoginName = this.assignData.user.expected_login_name; 
         cy.login(email, password);
-        cy.get('#nav-link-accountList-nav-line-1').should('contain', expectedLoginName);    
+        cy.get(searchPage.locators.login_name).should('contain', expectedLoginName);    
     })
 
     it('Search for a product', function() {
@@ -32,9 +32,52 @@ describe('Assignment 1', () => {
         searchPage.getSuggestionContainer().should('be.visible');
         searchPage.getSuggestionContainer().find(searchPage.locators.suggestion_list).should('contain', searchText);
         searchPage.clickSearchButton();
-        searchPage.verifySearchPage(expectedText);
-        searchPage.verifySearchListLength();
+        searchPage.getSearchPage().should('have.text', expectedText);;
+        searchPage.getSearchList().should('have.length.gte', 15);
 
+        const expected_min_price =  this.assignData.search.expected_min_price;
+        const expected_max_price =  this.assignData.search.expected_max_price;
+        cy.log(`Expected min price: ${expected_min_price}`);
+        cy.log(`Expected max price: ${expected_max_price}`);
+        searchPage.setPriceRange(this.assignData.search.min_price_slider, this.assignData.search.max_price_slider);  
+        cy.screenshot('price-range-slider');
+        searchPage.getProductPrices().each(($price) => {
+        
+        const price = parseInt($price.text().replace(/,/g, ''), 10);
+        expect(price).to.be.gte(expected_min_price);
+        expect(price).to.be.lte(expected_max_price);
+        })
+        const brand = this.assignData.search.filter_brand;
+        searchPage.selectBrand(brand);
+        searchPage.validateProductsContainBrand(brand);
+
+    })
+    it('Verify specific product', function() {
+        cy.login(email, password);
+        const product_name = this.assignData.product.product_name;
+        const expected_product_price = this.assignData.product.product_price;
+        
+        searchPage.enterSearchText(product_name);
+        searchPage.clickSearchButton();
+        searchPage.getSearchPage().should('contain', product_name);
+        searchPage.getProductList().each(($product) => {
+            const productText = $product.text().toLowerCase();
+            cy.log(`searching for ${productText}`);
+            const expected_product_name = product_name.toLowerCase();
+            
+            if (productText.includes(expected_product_name)) {
+                cy.wrap($product).parent('a').invoke('removeAttr','target').click({timeout: 5000});
+                return false; 
+            }
+        });
+        // searchPage.getProductList().contains(product_name).parent('a').invoke('removeAttr','target').click({force:true});
+        
+        searchPage.getSpecificProductTitle().should('contain', product_name);
+        searchPage.getSpecificProductPrice().should('be.visible');
+        searchPage.getSpecificProductPrice().invoke('text').then(($price) => {
+            const price = parseInt($price.replace(/,/g, ''), 10);
+            expect(price).to.eq(expected_product_price);
+        })
     })
     
 
--- cypress/support/SearchPage.js
+++ cypress/support/SearchPage.js
@@ -1,17 +1,28 @@
 export class SearchPage {
     
         locators = {
+            login_name: '#nav-link-accountList-nav-line-1',
             searchBox: 'input#twotabsearchtextbox',
             searchButton: '#nav-search-submit-button',
             suggestion_container:'div.s-suggestion-container',
             suggestion_list:'div.s-suggestion',
             search_result:'span.a-color-state.a-text-bold',
             search_list:'h2.a-size-medium.a-spacing-none.a-color-base.a-text-normal',
-
-        };
+            slider_min: 'input[id="p_36/range-slider_slider-item_lower-bound-slider"]',
+            slider_max: 'input[id="p_36/range-slider_slider-item_upper-bound-slider"]',
+            product_price: 'span.a-price-whole',
+            brand_checkbox:'span.a-size-base.a-color-base',
+            main_container:'div.s-main-slot',
+            product_list:'div[data-cy="title-recipe"] h2 span',
+            products:'h2.a-size-medium span',
+            product_title:'h1 span#productTitle',
+            buy_now_button:'input#buy-now-button',
+        }
         
 
-   
+   verifyLoginName(expectedText) {
+        cy.get(this.locators.login_name).should('have.text', expectedText);
+    }
     // Action Methods
     enterSearchText(text) {
         cy.get(this.locators.searchBox).type(text);
@@ -24,10 +35,42 @@ export class SearchPage {
         cy.get(this.locators.searchButton).click();
     }
 
-    verifySearchPage(expectedText) {
-       cy.get(this.locators.search_result).should('have.text', expectedText);
+    getSearchPage() {
+       return cy.get(this.locators.search_result);
     }
-    verifySearchListLength() {
-        cy.get(this.locators.search_list).should('have.length.gte', 15);
+    getSearchList() {
+        return cy.get(this.locators.search_list);
     }
+
+    setPriceRange(min, max) {
+        cy.get(this.locators.slider_min,{ timeout: 5000 }).invoke('val', min).trigger('change',{force: true});
+        cy.get(this.locators.slider_max,{ timeout: 5000 }).invoke('val', max).trigger('change',{force: true});
+    }
+    getProductPrices() {
+        
+        return cy.get(this.locators.product_price);
+    }
+
+    selectBrand(brand) {
+        cy.get(this.locators.brand_checkbox).contains(brand).click();
+        cy.get(this.locators.main_container, { timeout: 10000 }).should('be.visible');
+    }
+    validateProductsContainBrand(brand) {
+        
+        cy.get(this.locators.product_list).each(($product) => {
+            const productText = $product.text().toLowerCase();
+            expect(productText).to.include(brand.toLowerCase());
+        });
+    }
+    getProductList() {
+        return cy.get(this.locators.products);
+
+    }
+    getSpecificProductTitle() {
+        return cy.get(this.locators.product_title);
+    }
+    getSpecificProductPrice() {
+        return cy.get(this.locators.product_price);
+    }
+
 }
