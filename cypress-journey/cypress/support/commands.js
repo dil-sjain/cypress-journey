6058133cae18757b648bc2270812cdb9e7c3356a
dil-sjain <sjain@diligent.com>
2025-05-06 09:35:33 +0530

    added login custom command

--- cypress-journey/cypress/support/commands.js
+++ cypress-journey/cypress/support/commands.js
@@ -1,1 +1,14 @@
-// This file is intentionally left blank.
+import loginPage from "../support/pageObjects/loginPage";
+
+Cypress.Commands.add('loginInToApplication', (username, password) => {
+    cy.log('Logging in', Cypress.env('BASE_URL'));
+    cy.clearCookies();
+
+    cy.visit(Cypress.env('BASE_URL'), { timeout: 10000 });
+
+    loginPage.enterUsername(username);
+    loginPage.enterPassword(password);
+    loginPage.clickOnLoginButton();
+
+    WaitUtils.waitForGivenTime(2);
+});
---
+++ cypress-journey/cypress/support/pageObjects/addToCartPage.js
@@ -0,0 +1,11 @@
+class addToCartPage {
+    webLocators = {
+      //formControl: () => cy.get('.form-control'),
+    };
+
+    // getSearchInput() {
+    //     return this.webLocators.formControl();
+    // }
+}
+
+export default new addToCartPage();
