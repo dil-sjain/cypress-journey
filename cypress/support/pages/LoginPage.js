b63742e7fe7ed51b8634eaeac2f40efcab5bb1a2
dil-agiddappa <agiddappa@diligent.com>
2025-05-07 10:43:02 +0530

    Amazon login and search

---
+++ cypress/support/pages/LoginPage.js
@@ -0,0 +1,37 @@
+class LoginPage {
+    visit() {
+        cy.visit('/');
+    }
+
+    enterUsername(username) {
+        cy.get('.nav-action-inner').click({force:true})
+        cy.get('#ap_email_login').type(username);
+        cy.get('input[type="submit"]').first()
+
+    }
+
+    enterPassword(password) {
+        cy.get('#ap_password').type(password);
+    }
+
+    clickLogin() {
+        cy.get('#login-button').click();
+    }
+    loginwithCreds(username, password) {
+        this.visit();
+        this.enterUsername(username);
+        this.enterPassword(password);
+        this.clickLogin();
+    }
+    assertLoginSuccess() {
+        cy.url().should('include', '/dashboard');
+        cy.contains('Welcome').should('be.visible');
+    }
+    assertLoginFailure() {
+        cy.contains('Invalid credentials').should('be.visible');
+    }
+}
+
+const login = new LoginPage();
+
+export default login
