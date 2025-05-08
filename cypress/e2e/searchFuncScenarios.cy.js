75d9192748a24c68fd545671334a33b73dd1b2ab
kravichandrababu <k.ravichandrababu@globallogic.com>
2025-05-08 06:52:59 +0530

    previously some test's are commented

--- cypress/e2e/searchFuncScenarios.cy.js
+++ cypress/e2e/searchFuncScenarios.cy.js
@@ -13,25 +13,25 @@ describe('Sample Test Suite', () => {
     });
 
 
-    // it('Search Operation without Login and Printing Productname and Prizes', () => {
-    //     var reqProduct=Cypress.env('productName')[0]
-    //     wellcomepage.webLocators.getSearchTxbOnWellcome().clear().type(`${reqProduct}{enter}`)
-    //     cy.wait(2000)
-    //     cy.get('h2[aria-label*="Apple iPhone 15"]').each(($el) => {
-    //             const Mobilename= $el.text()
-    //             const prize=$el.closest('div.a-spacing-top-small').find('span.a-price-whole').text()
-    //             cy.log(Mobilename+"      "+prize) 
-    //     })
-    // });
+    it('Search Operation without Login and Printing Productname and Prizes', () => {
+        var reqProduct=Cypress.env('productName')[0]
+        wellcomepage.webLocators.getSearchTxbOnWellcome().clear().type(`${reqProduct}{enter}`)
+        cy.wait(2000)
+        cy.get('h2[aria-label*="Apple iPhone 15"]').each(($el) => {
+                const Mobilename= $el.text()
+                const prize=$el.closest('div.a-spacing-top-small').find('span.a-price-whole').text()
+                cy.log(Mobilename+"      "+prize) 
+        })
+    });
 
-    // it('Search Operation without Login and Selecting the 1st Matching Element', () => {
-    //     var reqProduct=Cypress.env('productName')[0]
-    //     wellcomepage.webLocators.getSearchTxbOnWellcome().clear().type(`${reqProduct}`).type('{enter}')//.type(`${reqProduct}{enter}`);
-    //     cy.wait(2000)
-    //     cy.get('h2[aria-label*="Apple iPhone 15"]').first().click()
-    //     cy.log('User has been clicked on 1st Matching Element in the list ')
+    it('Search Operation without Login and Selecting the 1st Matching Element', () => {
+        var reqProduct=Cypress.env('productName')[0]
+        wellcomepage.webLocators.getSearchTxbOnWellcome().clear().type(`${reqProduct}`).type('{enter}')//.type(`${reqProduct}{enter}`);
+        cy.wait(2000)
+        cy.get('h2[aria-label*="Apple iPhone 15"]').first().click()
+        cy.log('User has been clicked on 1st Matching Element in the list ')
 
-    //     })
+        })
 
     it('Adding the product to the cart and validating', () => {
         wellcomepage.webLocators.getDirectSignInbtn().click()
