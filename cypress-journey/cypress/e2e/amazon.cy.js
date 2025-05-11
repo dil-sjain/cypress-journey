9ecb1cb2ad6dffff72179424355951e584fdebb5
sshukla <sshukla@diligent.com>
2025-05-12 03:03:30 +0530

    Updated code with intercept and stubbing

--- cypress-journey/cypress/e2e/amazon.cy.js
+++ cypress-journey/cypress/e2e/amazon.cy.js
@@ -21,19 +21,46 @@ describe('amazon shopping cart', () => {
         cy.get('.nav-search-field input', { timeout: 10000 }).should('be.visible');
     });
 
-    it('search product and add to cart', function () {
+    it('search product and add to cart with intercept', function () {
+        cy.intercept('POST', '**/com.amazon.csm.csa.prod', {
+            statusCode: 200,
+            body: {
+                results: [
+                    { name: 'Sony PlayStation5 Gaming Console (Slim)', price: '₹49,990' }
+                ],
+            },
+        }).as('searchAPI');
         searchpage.searchProduct(this.data.product);
+        cy.wait('@searchAPI').its('response.statusCode').should('eq', 200);
         addtocart.openAndAddtoCart();
     });
 
-    it('Search results are same irrespective of case sensitivity', function () {
+    it.skip('Search results are same irrespective of case sensitivity', function () {
+        cy.intercept('POST', '**/com.amazon.csm.csa.prod', {
+            statusCode: 200,
+            body: {
+                results: [
+                    { name: 'Sony PlayStation5 Gaming Console (Slim)', price: '₹49,990' },
+                ],
+            },
+        }).as('searchAPI');
+
         searchpage.searchProduct(this.data.product);
+
+        cy.wait('@searchAPI').its('response.statusCode').should('eq', 200);
+
         searchpage.searchCapsOfAndCapsOn();
     });
 
     it('cart validation - check cart count increases', function () {
+        cy.intercept('POST', '**/https://unagi-eu.amazon.com/1/events/com.amazon.csm.nexusclient.prod', {
+            statusCode: 200,
+            body: { cartCount: 2 },
+        }).as('cartAPI');
+
         cartpage.getInitialCartCount().then((initialCount) => {
             searchpage.searchProduct(this.data.watch);
+            cy.wait('@cartAPI').its('response.statusCode').should('eq', 200);
             addtocart.openWatchAndAddtoCart();
             cartpage.confirmNoCoverage();
             cartpage.confirmCartCountIncreased(initialCount);
