54dc9cf472b8dc7e4cfd948ed808c40cddfd3296
Simran Arora <siarora@diligent.com>
2025-05-12 18:43:11 +0530

    Assignment-2

--- cypress-journey/cypress/support/pageObjects/addToCartPage.js
+++ cypress-journey/cypress/support/pageObjects/addToCartPage.js
@@ -1,11 +1,101 @@
 class addToCartPage {
-    webLocators = {
-      //formControl: () => cy.get('.form-control'),
-    };
+  webLocators = {
+    addToCartButton: () => cy.get("#a-autoid-1-announce"),
+    cartCount: () => cy.get("#nav-cart-count"),
+    proceedToCheckoutButton: () =>
+      cy.get(
+        "#ewc-compact-actions-container > div > div.a-row.a-spacing-base.a-spacing-top-small.ewc-go-to-cart.celwidget > span > span > a"
+      ),
+    proceedToBuy: () => cy.get("#sc-buy-box-ptc-button > span > input"),
+    cartItems: () => cy.get(".a-row.sc-list-item.sc-java-remote-feature"),
+    deliveryAddressText: () => cy.get("#deliver-to-address-text"),
+    totalAmountText: () => cy.get("#sc-subtotal-amount-buybox > span"),
+    clickOnCart: () => cy.get("#nav-cart-count-container"),
+  };
 
-    // getSearchInput() {
-    //     return this.webLocators.formControl();
-    // }
+  openAndAddtoCart() {
+    this.webLocators.addToCartButton().eq(0).click({ force: true });
+  }
+
+  cartPersistAcrossSession(initialCartCount) {
+    expect(initialCartCount).to.be.greaterThan(0);
+  }
+
+  getInitialCartCount() {
+    return this.webLocators
+      .cartCount()
+      .invoke("text")
+      .then((text) => parseInt(text));
+  }
+
+  getTotalFromCartItem() {
+    let totalCartValue = 0;
+    return this.webLocators
+      .cartItems()
+      .each((item) => {
+        cy.wrap(item)
+          .invoke("attr", "data-subtotal")
+          .then((subtotal) => {
+            totalCartValue += JSON.parse(subtotal).subtotal.amount;
+          });
+      })
+      .then(() => Promise.resolve(totalCartValue));
+  }
+
+  confirmCartCountIncreased(initialCount) {
+    cy.wait(2000);
+    this.webLocators
+      .cartCount({ timeout: 10000 })
+      .invoke("text")
+      .then((newCount) => {
+        const updatedCount = parseInt(newCount);
+        expect(updatedCount).to.be.greaterThan(initialCount);
+      });
+  }
+
+  clickProceedToBuy() {
+    this.webLocators.proceedToBuy().click();
+  }
+
+  proceedToCheckout() {
+    this.webLocators.proceedToCheckoutButton().click();
+  }
+
+  verifyAddress(expectedAddress) {
+    this.webLocators
+      .deliveryAddressText()
+      .should("contain.text", expectedAddress);
+  }
+
+  verifyTotalAmount(expectedAmount) {
+    this.webLocators
+      .totalAmountText()
+      .invoke("text")
+      .then((totalAmount) => {
+        const parsedAmount = parseInt(
+          totalAmount.substring(1).replace(/,/g, "")
+        );
+        expect(parsedAmount).to.equal(expectedAmount);
+      });
+  }
+
+  removeItemFromCart() {
+    this.webLocators.clickOnCart().click();
+    this.webLocators
+      .cartItems()
+      .eq(0)
+      .find("span.a-size-small.sc-action-delete > span > input")
+      .click();
+  }
+
+  saveForLater() {
+    this.webLocators.clickOnCart().click();
+    this.webLocators
+      .cartItems()
+      .eq(0)
+      .find("span.a-size-small.sc-action-save-for-later > span > input")
+      .eq(0)
+      .click();
+  }
 }
-
 export default new addToCartPage();
--- cypress-journey/cypress/support/pageObjects/searchPage.js
+++ cypress-journey/cypress/support/pageObjects/searchPage.js
@@ -1,11 +1,43 @@
 class searchPage {
-    webLocators = {
-      //formControl: () => cy.get('.form-control'),
-    };
+  webLocators = {
+    searchInput: () => cy.get("#twotabsearchtextbox"),
+    searchResult: () => cy.get('[data-component-type="s-search-result"]'),
+    resultTitle: () => cy.get('div[data-cy="title-recipe"]'),
+    filterResult: () => cy.get('li[id="p_123/230542"] input[type="checkbox"]'),
+    noResultText: () => cy.get(".a-section > .s-no-outline > :nth-child(1)"),
+  };
 
-    // getSearchInput() {
-    //     return this.webLocators.formControl();
-    // }
+  searchProduct(productName) {
+    this.webLocators.searchInput().type(`${productName}{enter}`);
+  }
+
+  assertResultAndTitle(title) {
+    this.webLocators.searchResult().should("have.length.greaterThan", 5);
+    this.webLocators.resultTitle().should("include.text", title);
+  }
+
+  filterSearchResult(title2) {
+    this.webLocators.filterResult().check({ force: true });
+    this.webLocators.resultTitle().should("include.text", title2);
+  }
+
+  clearSearchBox() {
+    this.webLocators.searchInput().clear();
+  }
+
+  selectProduct() {
+    this.webLocators
+      .searchResult()
+      .first()
+      .find("img")
+      .parents("a")
+      .invoke("removeAttr", "target")
+      .click();
+  }
+
+  invalidResultSearch() {
+    this.webLocators.noResultText().should("contain.text", "No results");
+  }
 }
 
 export default new searchPage();
